8637ba9c45096420f97953dc00b5c4f7197c949b
Talgat <accurate19@mail.ru>
2024-08-16 15:09:07 +0300

    new Class

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,16 @@
 package app.config;
 
+import app.model.Timer;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
+@Component
+@Qualifier
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
 }
